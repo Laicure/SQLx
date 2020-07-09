1ce85c1a5888225663c9f5234631f4c6f70ac300
Laicure <Laicure@outlook.com>
2020-07-09 17:07:37 +0800

    SQLite update

--- SQLx/My Project/AssemblyInfo.vb
+++ SQLx/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2020.4.29.15037")>
+<Assembly: AssemblyVersion("2020.7.9.16398")>
 <Assembly: Resources.NeutralResourcesLanguageAttribute("en")>
