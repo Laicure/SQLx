d5fa2ee920cf3b66b404fd84dd01349d30bf869a
Laicure <Laicure@outlook.com>
2020-12-22 17:08:25 +0800

    SQLite update + upgrade to .NET 4.7.2

--- SQLx/My Project/AssemblyInfo.vb
+++ SQLx/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2020.7.9.16398")>
+<Assembly: AssemblyVersion("2020.12.22.16437")>
 <Assembly: Resources.NeutralResourcesLanguageAttribute("en")>
